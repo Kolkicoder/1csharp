346f203792427204e3a5fb30ec47026cd26f2ae1
Matej Kolkus <kolkus.m10@gmail.com>
2025-09-10 11:41:46 +0200

    opravil som 2

--- src/AppsLab-002-ConsoleWriteLine/Program.cs
+++ src/AppsLab-002-ConsoleWriteLine/Program.cs
@@ -1,1 +1,6 @@
-﻿Console.WriteLine("Hello, World!");
+﻿int age = 14;
+string name = "Matej";
+Console.WriteLine(age);
+Console.WriteLine(name);
+
+Console.WriteLine($"My name is {name} and I am {age} years old.");
